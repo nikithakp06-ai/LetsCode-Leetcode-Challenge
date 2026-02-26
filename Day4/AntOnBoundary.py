7fa46925c427cee0eac2e61f2ceadac53f4cdb21
nikithakp06-ai <nikithakp06@gmail.com>
2026-02-26 20:41:12 +0530

    Create AntOnBoundary.py

---
+++ Day4/AntOnBoundary.py
@@ -0,0 +1,10 @@
+class Solution:
+    def returnToBoundaryCount(self, nums: List[int]) -> int:
+        position = 0
+        count = 0
+        for num in nums:
+           position += num
+           if position == 0:
+              count += 1
+        return count
+        
